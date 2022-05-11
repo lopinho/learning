f7a348a9fb6e020f12e9d2162bfd2e62a9c9914a
André Ferreira <lopinho@gmail.com>
2022-05-11 15:24:15 -0300

    Adicionando mycv app, com servico completo em usuarios

---
+++ udemy/nestjs-cdg/mycv/src/users/users.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UsersService } from './users.service';
+
+@Controller('auth')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Post('/signup')
+  sigUp(@Body() body: CreateUserDto) {
+    this.userService.create(body.email, body.passowrd);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.userService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+  @Delete('/:id')
+  async deleteUser(@Param('id') id: string) {
+    return await this.userService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userService.update(parseInt(id), body);
+  }
+}
